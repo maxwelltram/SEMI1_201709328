3afff9b348d4f16ab4825c0cc22f52b5fe14d131
Maxwellt Ramírez <maxwelltjrr99@gmail.com>
2024-02-27 21:51:49 -0600

    a
    a

--- HT1/api.py
+++ HT1/api.py
@@ -15,4 +15,4 @@ def index():
   })
 
 if __name__ == '__main__':
-  app.run(debug=True, port=3000)  # Agrega la opción 'port=3000' aquí
+  app.run(debug=True,host='0.0.0.0', port=3000)  # Agrega la opción 'port=3000' aquí
